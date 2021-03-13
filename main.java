4b53379290db1437152093e608f364e6e35b2444
Екатерина Сочкова <volontare@yandex.ru>
2021-03-13 21:48:37 +0400

    Added main.java

---
+++ main.java
@@ -0,0 +1,8 @@
+public class main {
+    public static void main(String[] args) {
+        int balance = 2_000_000_000;
+        int add = 500_000_000;
+        int total = balance + add;
+        System.out.println(total);
+    }
+}
